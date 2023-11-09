4c7b81f128d4ed0760b27bb3f02e437ac25ba39d
Ramiro Ochoa <ramiro.ochoa@bg.com.bo>
2023-11-09 14:58:38 -0400

    init

---
+++ smart-exchange-back/src/main/java/com/qhatuna/exchange/SmartExchangeBackApplication.java
@@ -0,0 +1,13 @@
+package com.qhatuna.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExchangeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExchangeBackApplication.class, args);
+	}
+
+}
---
+++ smart-exchange-back/src/test/java/com/qhatuna/exchange/SmartExchangeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qhatuna.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExchangeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
